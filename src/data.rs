dc76211732a95a8d6775229c6cfcc97956824c24
Johannes Boczek <johannes.boczek@web.de>
2018-06-14 09:47:24 +0200

    Rename data fields and add comments

--- src/data.rs
+++ src/data.rs
@@ -4,6 +4,9 @@ use serde_json::{
 	Map
 };
 
+/*
+ * Struct holding all the static data regarding AoE 2.
+ */
 pub struct GameData {
 	pub buildings: Vec<Building>,
 	pub civs: Vec<Civ>,
@@ -13,10 +16,9 @@ pub struct GameData {
 }
 
 #[derive(Serialize, Deserialize)]
-#[allow(non_snake_case)]
 pub struct Building {
 	#[serde(rename = "type")]
-	pub type_name: String,
+	pub kind: String,
 	pub name: String,
 	#[serde(rename = "ver")]
 	pub game_version: String,
@@ -33,32 +35,39 @@ pub struct Building {
 	pub at: String,//???
 	#[serde(rename = "ar")]
 	pub armor: String,
-	pub GA: String,//Something with garrisoning
+	#[serde(rename = "GA")]
+	pub ga: String,//Something with garrisoning
 	#[serde(rename = "civb")]
 	pub civ_bonus: Option<Map<String, Value>>,
 	pub t: String,//Same as "avail"?
-	pub avail: Option<Vec<String>>,
-	pub noavail: Option<Vec<String>>
+	#[serde(rename = "avail")]
+	pub available_to: Option<Vec<String>>,
+	#[serde(rename = "noavail")]
+	pub not_available_to: Option<Vec<String>>
 }
 
 #[derive(Serialize, Deserialize)]
-#[allow(non_snake_case)]
 pub struct Civ {
 	pub name: String,
-	pub ver: String,
-	pub ct: String,
-	pub uu: String,
-	pub ut: String,
-	pub tb: String,
-	pub bs: String,
-	pub tt: String
+	#[serde(rename = "ver")]
+	pub game_version: String,
+	#[serde(rename = "ct")]
+	pub strength: String,
+	#[serde(rename = "uu")]
+	pub unique_unit: String,
+	#[serde(rename = "ut")]
+	pub unique_tech: String,
+	#[serde(rename = "tb")]
+	pub team_bonus: String,
+	#[serde(rename = "bs")]
+	pub boni: String,
+	pub tt: String//Kinda like the name, but again?
 }
 
 #[derive(Serialize, Deserialize)]
-#[allow(non_snake_case)]
 pub struct GatheringData {
 	#[serde(rename = "type")]
-	pub type_name: String,
+	pub resource: String,
 	pub source: String,
 	pub speed: String,
 	pub note: String
@@ -70,45 +79,57 @@ pub struct Tech {
 	#[serde(rename = "type")]
 	pub type_name: String,
 	pub name: String,
-	pub ver: String,
+	#[serde(rename = "ver")]
+	pub game_version: String,
 	pub age: String,
 	pub cost: String,
 	#[serde(rename = "for")]
-	pub for_name: String,
+	pub for_what: String,
 	pub time: String,
 	#[serde(rename = "civb")]
 	pub civ_bonus: Option<Map<String, Value>>,
-	pub t: String,
-	pub avail: Option<Vec<String>>,
-	pub noavail: Option<Vec<String>>
+	pub t: String,//Save as "avail"?
+	#[serde(rename = "avail")]
+	pub available_to: Option<Vec<String>>,
+	#[serde(rename = "noavail")]
+	pub not_available_to: Option<Vec<String>>
 }
 
 #[derive(Serialize, Deserialize)]
 #[allow(non_snake_case)]
 pub struct Unit {
 	#[serde(rename = "type")]
-	pub type_name: String,
+	pub kind: String,
 	pub name: String,
-	pub ver: String,
+	#[serde(rename = "ver")]
+	pub game_version: String,
 	pub age: String,
 	pub cost: String,
-	pub bt: String,
-	pub fr: String,
-	pub ad: String,
-	pub mr: String,
-	pub los: String,
+	#[serde(rename = "bt")]
+	pub time: String,
+	pub fr: String,//???
+	pub ad: String,//???
+	pub mr: String,//Movement speed?
+	#[serde(rename = "los")]
+	pub line_of_sight: String,
 	pub hp: String,
-	pub ra: String,
-	pub at: String,
-	pub ar: String,
+	#[serde(rename = "ra")]
+	pub range: String,
+	#[serde(rename = "at")]
+	pub attack: String,
+	#[serde(rename = "ar")]
+	pub armor: String,
 	pub note: String,
 	pub extra: Option<Map<String, Value>>,
 	#[serde(rename = "civb")]
 	pub civ_bonus: Option<Map<String, Value>>,
-	pub t: String
+	pub t: String//Same as "avail"?
 }
 
 impl GameData {
+	/*
+	 * Parses the given JSON data into the structs above.
+	 */
 	pub fn new<S, T, U, V, W>(building_data: S, civ_data: T, gathering_data: U, tech_data: V, unit_data: W) -> Self where S: AsRef<str>, T: AsRef<str>, U: AsRef<str>, V: AsRef<str>, W: AsRef<str> {
 		let buildings: Vec<Building> = serde_json::from_str(building_data.as_ref()).expect("Buildings JSON");
 		let civs: Vec<Civ> = serde_json::from_str(civ_data.as_ref()).expect("Civs JSON");
